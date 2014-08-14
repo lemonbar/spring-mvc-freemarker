cca6fedf02c414d7a99de403c06b7695d574bba4
limengbar <limengabc@163.com>
2014-08-14 10:45:51 +0800

    initial project

---
+++ src/main/java/com/lemon/spring/controller/GreetingController.java
@@ -0,0 +1,23 @@
+/* 
+ * Copyright (c) 2014 General Electric Company. All rights reserved. 
+ * 
+ * The copyright to the computer software herein is the property of 
+ * General Electric Company. The software may be used and/or copied only 
+ * with the written permission of General Electric Company or in accordance 
+ * with the terms and conditions stipulated in the agreement/contract 
+ * under which the software has been supplied. 
+ */
+package com.lemon.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GreetingController {
+
+    @RequestMapping("/greeting")
+    public String greeting(Model model){
+        return "welcome";
+    }
+}
---
+++ src/main/webapp/WEB-INF/ftl/welcome.ftl
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>Welcome!</title>
+</head>
+<body>
+<h1>Welcome lemon!</h1>
+</body>
+</html>
